94470c3047c11f74124d9ad38182335d3c148c9e
Camilo López <newflare@Camilos-Mac-mini.local>
2024-01-10 14:13:28 -0300

    [CONFIG]: Configured Bootstrap into the project

---
+++ src/pages/_document.jsx
@@ -0,0 +1,60 @@
+// Next.js
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript
+} from 'next/document';
+
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => App,
+        enhanceComponent: (Component) => Component,
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css'
+            rel='stylesheet'
+            integrity='sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1'
+            crossOrigin='anonymous'
+          />
+
+          <link
+            href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+            rel='stylesheet'
+            integrity='sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=='
+            crossOrigin='anonymous'
+            referrerPolicy='no-referrer'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+
+        <script
+          async
+          src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js'
+          integrity='sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW'
+          crossOrigin='anonymous'
+        />
+      </Html>
+    )
+  }
+}
+
+export default MyDocument;
